1e7e6e99a8da75a3aea038dc75005b0b94ae5d94
Mihailov <michhor8@gmail.com>
2022-06-29 19:09:30 +0200

    Jednoduche overeni existence obrazku

--- cypress/integration/bughunters/bughunters.js
+++ cypress/integration/bughunters/bughunters.js
@@ -3,14 +3,35 @@
 
 
 
-it('obrazek', () => {
+it('nacteni Webu', () => {
 
     cy.visit('/');
-    cy.get('.nav-menu > .page-item-556 > a')
-        .click()
     
+    cy.get('.nav-menu > .page-item-556 > a')
+        .click();
 
+        
+});
 
+it('test management', () => {
 
+    cy.get('.elementor-element-624ab7c > .elementor-widget-wrap > .elementor-element > .elementor-widget-container > .elementor-icon-box-wrapper > .elementor-icon-box-icon > .elementor-icon') 
+        .should('be.visible')
+
+});
+
+it('externi spoluprace', () => {
+    cy.get('.elementor-element-7d0a711 > .elementor-widget-container > .elementor-icon-box-wrapper > .elementor-icon-box-icon > .elementor-icon')
+           .should('be.visible')
+           
+
+});
+
+it('jeden tym', () => {
+
+    cy.get('.elementor-element-85da123 > .elementor-widget-container > .elementor-icon-box-wrapper > .elementor-icon-box-icon > .elementor-icon')
+        .should('be.visible')
+           
+           
 
 });
